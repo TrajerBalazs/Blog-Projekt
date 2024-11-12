1ea755875a0600b6d687554ae1c0a9d2e5adbb90
Balazs <trajerbazsi@gmail.com>
2024-11-12 20:08:42 +0100

    API URL módosítása a Render URL-re

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Állítsd be a Render URL-t az API végpont eléréséhez
-const apiUrl = "https://blog-projekt.onrender.com";
+const apiUrl = 'https://blog-projekt.onrender.com/api/posts';
 
 // A többi kód változatlan marad...
 
